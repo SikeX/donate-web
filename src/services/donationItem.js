b56f74648d7b82f218458317efda4062715faaba
sikex <yoloxz719@gmail.com>
2021-12-06 16:55:24 +0800

    feat: 设置获取banner图api

---
+++ src/services/donationItem.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+import { BASE_URL, FILE_BASE_URL } from "./api";
+
+const baseUrl = BASE_URL
+const file_base_url = FILE_BASE_URL
+
+const getAllItem = async () => {
+    const result = await axios.get(baseUrl+'donationItem/donationItem/list')
+    return result.data
+}
+
+const getAll = async () => {
+    const result = await axios.get(baseUrl)
+    return result.data
+}
+
+const getTodoList = async (todo) => {
+    const result = await axios.get(`${baseUrl}/${todo}`)
+    return result.data
+}
+
+const postTaskList = async (task) => {
+    const result = axios.post(`${baseUrl}/Tasks`,task)
+    return result
+}
+
+const postTodo = async (taskName,todo) => {
+    const result = axios.post(`${baseUrl}/Tasks/${taskName}`,todo)
+    return result
+}
+
+const deleteTodo = async (taskName,todoName) => {
+    const result = axios.delete(`${baseUrl}/Tasks/${taskName}`,{data:{name:todoName}})
+    return result
+}
+
+export default {
+    getAllItem: getAllItem
+}
